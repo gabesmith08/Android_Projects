45ca1bf6218cdb7833bb89d3678ca0c7edfac6ee
gsmith7 <gsmith7@iastate.edu>
2019-02-23 20:17:42 -0600

    start of weather app, uploading rest of project to weather app tomorrow.

---
+++ Storm/app/src/main/java/com/example/storm/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.storm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "187aa68d4c757b6f776d3a83393f2120";
+
+        double latitude = 41.654670;
+        double longitude = -90.583000;
+
+        String forecastURL = "https://api.darksky.net/forecast/"
+                   + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkhttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception caught: ", e);
+        }
+    }
+}
